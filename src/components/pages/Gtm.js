0782e0a31d8a7c9b06250ba63ee0c5b9fbbfd571
Jason Nam <neep305@gmail.com>
2025-01-14 21:01:12 +0900

    modify the GTM page

--- src/components/pages/Gtm.js
+++ src/components/pages/Gtm.js
@@ -2,7 +2,10 @@ import React from "react";
 
 const Gtm = () => {
     return (
-        <h1>Gtm</h1>
+        <div className="p-8 max-w-3xl mx-auto">
+            <h1 className="text-2xl font-bold mb-4">Gtm</h1>
+            <p className="text-gray-600 mb-8">This is the Gtm page of the Singular React application.</p>
+        </div>
     );
 }
 
